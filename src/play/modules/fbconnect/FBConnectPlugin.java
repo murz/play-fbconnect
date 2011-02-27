b3f030928a3c167f32227135ad50a188439b3b61
smallufo <smallufo@gmail.com>
2011-02-27 23:38:46 +0800

    fix FBConnectPlugin.session() bug. In prod mode , ThreadLocal<FBConnectSession> may be null. Not sure why.

--- src/play/modules/fbconnect/FBConnectPlugin.java
+++ src/play/modules/fbconnect/FBConnectPlugin.java
@@ -1,26 +1,45 @@
 package play.modules.fbconnect;
 
+import play.Logger;
 import play.PlayPlugin;
 import play.mvc.Router;
 
-public class FBConnectPlugin extends PlayPlugin {
-    
-    private static ThreadLocal<FBConnectSession> _session = new ThreadLocal<FBConnectSession>();
-    
-    public FBConnectSession session(){
-        return _session.get();
+public class FBConnectPlugin extends PlayPlugin
+{
+
+  private static ThreadLocal<FBConnectSession> _session = new ThreadLocal<FBConnectSession>();
+
+  public FBConnectSession session()
+  {
+    FBConnectSession s = _session.get();
+    if (s != null)
+    {
+      Logger.trace("found session in threadLocal");
+      return s;
     }
-    
-    @Override
-    public void onApplicationStart() {
-        FBConnectSession session = new FBConnectSession();
-        session.init();
-        _session.set(session);
+    else
+    {
+      Logger.trace("NOT found session in threadLocal");
+      FBConnectSession session = new FBConnectSession();
+      session.init();
+      _session.set(session);
+      return _session.get();
     }
-    
-    @Override
-    public void onRoutesLoaded() {
-        Router.addRoute("GET", "/fbconnect/oauth/callback", "FBConnect.callback");
-    }
-    
+  }
+
+  @Override
+  public void onApplicationStart()
+  {
+    Logger.trace("FBConnectPlugin.onApplicationStart()");
+    FBConnectSession session = new FBConnectSession();
+    session.init();
+    _session.set(session);
+  }
+
+  @Override
+  public void onRoutesLoaded()
+  {
+    Router.addRoute("GET", "/fbconnect/oauth/callback", "FBConnect.callback");
+  }
+
 }
